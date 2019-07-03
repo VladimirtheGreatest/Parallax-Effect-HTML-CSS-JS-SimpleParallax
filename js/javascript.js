fa6667a2493d1b4c4e462acd1962457196d13e0b
Vladimir <48209956+VladimirtheGreatest@users.noreply.github.com>
2019-07-03 15:33:52 +0100

    Add files via upload
    JS+ images

---
+++ js/javascript.js
@@ -0,0 +1,28 @@
+var image = document.getElementsByClassName('one');
+new simpleParallax(image, {
+  scale: 1.8
+});
+var image = document.getElementsByClassName('two');
+new simpleParallax(image, {
+  scale: 1.6,
+  orientation: 'left'
+});
+var image = document.getElementsByClassName('three');
+new simpleParallax(image, {
+  scale: 1.6,
+  orientation: 'right'
+});
+var image = document.getElementsByClassName('four');
+new simpleParallax(image, {
+  scale: 1.6,
+});
+var image = document.getElementsByClassName('five');
+new simpleParallax(image, {
+  scale: 1.6,
+  orientation: 'right'
+});
+var image = document.getElementsByClassName('six');
+new simpleParallax(image, {
+  scale: 1.6,
+  orientation: 'left'
+});
