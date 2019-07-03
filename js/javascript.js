aa62964feab90345c549868a5d3e17bc25ea191c
Vladimir <48209956+VladimirtheGreatest@users.noreply.github.com>
2019-07-03 19:19:59 +0100

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -1,6 +1,7 @@
 var image = document.getElementsByClassName('one');
 new simpleParallax(image, {
-  scale: 1.8
+  scale: 1.6,
+  orientation: 'right',
 });
 var image = document.getElementsByClassName('two');
 new simpleParallax(image, {
@@ -9,17 +10,21 @@ new simpleParallax(image, {
 });
 var image = document.getElementsByClassName('three');
 new simpleParallax(image, {
-  scale: 1.6,
-  orientation: 'right'
+  scale: 1.9,
+
+  	overflow: true,
+    delay: 3.6,
+  transition: 'cubic-bezier(0,0,0,1)'
 });
 var image = document.getElementsByClassName('four');
 new simpleParallax(image, {
   scale: 1.6,
+  orientation: 'right'
 });
 var image = document.getElementsByClassName('five');
 new simpleParallax(image, {
   scale: 1.6,
-  orientation: 'right'
+  orientation: 'left'
 });
 var image = document.getElementsByClassName('six');
 new simpleParallax(image, {
