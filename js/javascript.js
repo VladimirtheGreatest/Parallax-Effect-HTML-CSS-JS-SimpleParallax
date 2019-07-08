cb357e1d9cf4ffe0bd54e04f233961c796fb62e6
Vladimir <48209956+VladimirtheGreatest@users.noreply.github.com>
2019-07-08 11:02:44 +0100

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -3,15 +3,13 @@
 var image = document.getElementsByClassName('three');
 new simpleParallax(image, {
   scale: 1.9,
-  	overflow: true,
-      orientation: ' down right',
-    delay: 3.6,
+  overflow: true,
+  orientation: ' down right',
+  delay: 3.6,
   transition: 'cubic-bezier(0,0,0,1)'
 });
 var image = document.getElementsByClassName('four');
-new simpleParallax(image, {
-  scale: 1.2,
-});
+new simpleParallax(image, {scale: 1.2});
 var image = document.getElementsByClassName('five');
 new simpleParallax(image, {
   scale: 1.5,
@@ -20,14 +18,13 @@ new simpleParallax(image, {
 var image = document.getElementsByClassName('six');
 new simpleParallax(image, {
   scale: 1.9,
-    overflow: true,
-    delay: 3.6,
-    orientation: 'right',
+  overflow: true,
+  delay: 3.6,
+  orientation: 'right',
   transition: 'cubic-bezier(0,0,0,1)'
 });
 
 //Simple parallax end
-
 
 // Customer greeting, playing with JavaScript ES6
 class Customer {
@@ -38,7 +35,7 @@ class Customer {
   }
 
   getName() {
-    return 'Your name is' + ' ' +  this.firstname + ' ' + this.lastname + ' ' + 'and you are' + ' ' + this.age + ' ' + 'years old' ;
+    return 'Your name is' + ' ' + this.firstname + ' ' + this.lastname + ' ' + 'and you are' + ' ' + this.age + ' ' + 'years old';
   }
 
   getGreetings() {
@@ -46,10 +43,18 @@ class Customer {
   }
 }
 
-  const Age = prompt("What is your age");
-  const getAge = () => Age;
-
+const Age = prompt("What is your age"); //change into input
+const getAge = () => Age;
+const UnderAge = (getAge() >= 18)
+  ? 'Hi'
+  : 'Leave';
 
 const John = new Customer('John', 'Baptist');
 console.log(John.getGreetings());
 console.log(John.getName());
+console.log(UnderAge);
+
+//Event listeners
+
+document.getElementById('entrance').addEventListener("click", open = () => document.getElementById('main').classList.add("newbody"));
+window.addEventListener("scroll", welcome = () => document.getElementById('entrance').innerHTML = "Welcome");
