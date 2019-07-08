c68991beee34c1c68a7cb7d47eef9f7232ac40fa
Vladimir <48209956+VladimirtheGreatest@users.noreply.github.com>
2019-07-08 06:51:33 +0100

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,5 @@
+//Simple parallax
+
 var image = document.getElementsByClassName('three');
 new simpleParallax(image, {
   scale: 1.9,
@@ -23,3 +25,31 @@ new simpleParallax(image, {
     orientation: 'right',
   transition: 'cubic-bezier(0,0,0,1)'
 });
+
+//Simple parallax end
+
+
+// Customer greeting, playing with JavaScript ES6
+class Customer {
+  constructor(firstname, lastname) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.age = getAge();
+  }
+
+  getName() {
+    return 'Your name is' + ' ' +  this.firstname + ' ' + this.lastname + ' ' + 'and you are' + ' ' + this.age + ' ' + 'years old' ;
+  }
+
+  getGreetings() {
+    return 'Hello';
+  }
+}
+
+  const Age = prompt("What is your age");
+  const getAge = () => Age;
+
+
+const John = new Customer('John', 'Baptist');
+console.log(John.getGreetings());
+console.log(John.getName());
